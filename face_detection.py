e062de622156953a66d71db6f7a356c98749ac0a
Satwiko Wirawan Indrawanto <satwikowirawan@live.com>
2020-11-11 02:31:13 +0700

    add function to read all images in a folder and write detected faces into new images

--- face_detection.py
+++ face_detection.py
@@ -1,26 +1,44 @@
+import os
 import numpy as np
 from cv2 import cv2
 
+def load_images_from_folder(folder):
+    images = []
+    for filename in os.listdir(folder):
+        img = cv2.imread(os.path.join(folder, filename))
+        if img is not None:
+            images.append(tuple((img, os.path.splitext(filename)[0])))
+    return images
+
+def face_recognition_from_image(image, image_number, net):
+    (h, w) = image.shape[:2]
+    blob = cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)), 1.0, (300, 300), (104.0, 177.0, 123.0))
+    
+    net.setInput(blob)
+    detections = net.forward()
+
+    for i in range(0, detections.shape[2]):
+        confidence = detections[0, 0, i, 2]
+
+        if confidence > 0.5:
+            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+            (startX, startY, endX, endY) = box.astype("int")
+
+            if startX <= w or startY <= h:
+                cv2.imwrite(os.path.join(save_path , 'image' + str(image_number) + '_face' + str(i) + '.jpg'), image[startY:endY, startX:endX])
+                # text = "{:.2f}%".format(confidence * 100)
+                # y = startY - 10 if startY - 10 > 10 else startY + 10
+                # cv2.rectangle(image, (startX, startY), (endX, endY), (0, 0, 255), 2)
+                # cv2.putText(image, text, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+    
+    # cv2.imshow("Display window", image)
+    # cv2.waitKey(0)
+
 net = cv2.dnn.readNetFromCaffe("deploy.prototxt.txt", "res10_300x300_ssd_iter_140000.caffemodel")
 
-image = cv2.imread("DSC_6228_resized2.JPG")
-(h, w) = image.shape[:2]
-blob = cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)), 1.0, (300, 300), (104.0, 177.0, 123.0))
+dataset_path = os.path.realpath(os.getcwd() + '/dataset')
+save_path = os.path.realpath(os.getcwd() + '/faces')
+images = load_images_from_folder(dataset_path)
 
-net.setInput(blob)
-detections = net.forward()
-
-for i in range(0, detections.shape[2]):
-    confidence = detections[0, 0, i, 2]
-
-    if confidence > 0.5:
-        box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-        (startX, startY, endX, endY) = box.astype("int")
-
-        text = "{:.2f}%".format(confidence * 100)
-        y = startY - 10 if startY - 10 > 10 else startY + 10
-        cv2.rectangle(image, (startX, startY), (endX, endY), (0, 0, 255), 2)
-        cv2.putText(image, text, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
-
-cv2.imshow("Display window", image)
-cv2.waitKey(0)
+for i in range(len(images)):
+    face_recognition_from_image(images[i][0], i, net)
